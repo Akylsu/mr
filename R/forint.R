6853c8e5906639e83f848a258f1fc4281dbb6e13
Akylsu <akylsu@gmail.com>
2020-05-13 17:32:19 +0000

    add convert_currency function

--- R/forint.R
+++ R/forint.R
@@ -16,3 +16,24 @@ get_bitcoin_price <- function(retried = 0) {
 #' @importFrom scales dollar
 forint <- function(x) {
   dollar(x, prefix = '', suffix = 'Ft')}
+
+#' Get exchange rates for a pair of currencies for a certain number of days
+#' @param base: base currency, symbol: equivalent in a given currency, days: number of days
+#' @export
+#' @importFrom data.table data.table
+#' @importFrom httr GET content
+#' @importFrom tidyr fill
+convert_currency <- function(base, symbol, days){
+  data <- content(GET(
+    "https://api.exchangeratesapi.io/history",
+    query = list(
+      start_at = Sys.Date() - days + 1,
+      end_at   = Sys.Date(),
+      base = paste0(base),
+      symbols = paste0(symbol)
+    )))$rates
+  ## days != actual number of days, skips non-working days
+  data <- data.table(date = as.Date(names(data)),
+                     value = unlist(data))[order(date)]
+  return(data)
+}
