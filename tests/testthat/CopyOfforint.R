df6aae37c5a01a325956cbc7ff2a5caa11cc22e5
Akylsu <akylsu@gmail.com>
2020-05-29 19:41:04 +0000

    set up CI

--- tests/testthat/CopyOfforint.R
+++ tests/testthat/CopyOfforint.R
@@ -1,7 +1,6 @@
 #' Access Binance API for BTC price in USD
 #' @export
-#' @param retried # the number of retries before sleep
-#' @return Return the latest price of 1 BTC in USD
+#' @param retied # the number of retries before sleep
 #' @importFrom binancer binance_coins_prices
 get_bitcoin_price <- function(retried = 0) {
   tryCatch(
@@ -19,26 +18,19 @@ forint <- function(x) {
   dollar(x, prefix = '', suffix = 'Ft')}
 
 #' Get exchange rates for a pair of currencies for a certain number of days
-#' @param base: base currency
-#' @param quote: quote currency
-#' @param days: number of days to return
-#' @examples
-#' convert_currency("USD", "JPY", 10)
-#' convert_currency("EUR", "HUF", 15)
-#' @return Table that indicates how much of the \code{quote} currency is needed to purchase one unit of
-#' the \code{base} currency for a given number of \code{days}
+#' @param base: base currency, symbol: equivalent in a given currency, days: number of days
 #' @export
 #' @importFrom data.table data.table
 #' @importFrom httr GET content
 #' @importFrom tidyr fill
-convert_currency <- function(base, quote, days){
+convert_currency <- function(base, symbol, days){
   data <- content(GET(
     "https://api.exchangeratesapi.io/history",
     query = list(
       start_at = Sys.Date() - days + 1,
       end_at   = Sys.Date(),
       base = paste0(base),
-      symbols = paste0(quote)
+      symbols = paste0(symbol)
     )))$rates
   ## days != actual number of days, skips non-working days
   data <- data.table(date = as.Date(names(data)),
